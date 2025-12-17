447a501f5ad9924d26e7a9b1a2475e6b884bb1fd
Hugo Gonzalez <hugo.andres9917@gmail.com>
2025-12-16 18:28:35 -0600

    modificaciones en ejeecicio 1 y ejercicios Big-O

--- past-exercises/semana15/ejercicio 1 semana15.py
+++ past-exercises/semana15/ejercicio 1 semana15.py
@@ -1,20 +1,24 @@
 #ejercicio 1 semana 15
 
 def bubble_sort(list_to_sort):
-    n = len(list_to_sort) #0(n)
+    n = len(list_to_sort) # O(n)
 
-    for i in range(n - 1):# 0(n)
-        for j in range(n -1- i):# 0(n^2)
-            current_element = list_to_sort[j] #0(n^2)
-            next_element = list_to_sort[j + 1]
+    for i in range(n - 1):# O(n)
+        swapped = False
 
-            print(f'--iteracion {i}, Elemento actual {current_element}, Segundo elemento{next_element}')# 0(n^2)
+        for j in range(n -1- i):# O(n^2)
+            current_element = list_to_sort[j] # O(1)
+            next_element = list_to_sort[j + 1] # O(1)
 
-            if current_element > next_element: #0(n^2)
-                list_to_sort[j], list_to_sort[j + 1] = list_to_sort[j + 1], list_to_sort[j] #0(n^2)
+            print(f'--iteracion {i}, Elemento actual {current_element}, Segundo elemento{next_element}')# 0(1)
 
+            if current_element > next_element: # O(1)
+                list_to_sort[j], list_to_sort[j + 1] = list_to_sort[j + 1], list_to_sort[j] # O(1)
+                swapped = True #O(1)
+        if not swapped:#O(n)
+            break #o(n)
 
-my_testing_list  = [2, 5, 7, 1, 4, 8, 3, 6, 9 ] #0(n)
+my_testing_list  = [1,2,3,4,5,6,7,8,9,0 ] #0(n)
 bubble_sort(my_testing_list) #0(n)
 
 print(my_testing_list)# 0(n)
