0750d95492f1302829803ede1641f15e1779b65a
Hugo Gonzalez <hugo.andres9917@gmail.com>
2026-06-11 13:30:08 -0600

    Agregando ejercicios de Algoritmos de Ordenamiento Bubble_sort

--- past-exercises/semana3/ejercicio 2.py
+++
@@ -1,4 +0,0 @@
-print(1+1)
-print(90+430)
-print(4*5)
-print(50/10)
--- past-exercises/semana3/ejercicio EXTRA1.py
+++
@@ -1,4 +0,0 @@
-print("Mi nombre es Hugo Gonzalez")
-print("Yo tengo 37 años")
-print("Mi color favorito es el Azul")
-print("Mi comida favorita es la Ceviche")
